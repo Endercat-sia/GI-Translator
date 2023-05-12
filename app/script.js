354598bf1009b78049d833230df94a297dac6786
Endercat末影猫 <119492185+Endercat-sia@users.noreply.github.com>
2023-05-12 19:09:26 +0800

    js update

--- app/script.js
+++ app/script.js
@@ -1,47 +1,46 @@
-fetch("items-list.json")
-			.then(response => response.json())
+fetch("items.json")
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(`Failed to load item data (${response.status} ${response.statusText})`);
+				}
+				return response.json();
+			})
 			.then(items => {
-
-		// Function to search for items
-		function searchItems(query) {
-			// Clear previous search results
-			document.getElementById("results").innerHTML = "";
-
-			// Filter items based on search query
-			const filteredItems = items.filter(item => item.id.toString().toLowerCase().includes(query.toLowerCase()) || item.name.toLowerCase().includes(query.toLowerCase()) || item.description.toLowerCase().includes(query.toLowerCase()) || item.tags.some(tag => tag.toLowerCase().includes(query.toLowerCase())));
-
-			// Display search results or feedback if no results found
-			if (filteredItems.length === 0) {
-				const listItem = document.createElement("li");
-				listItem.textContent = "No results found.";
-				document.getElementById("results").appendChild(listItem);
-			} else {
-				filteredItems.forEach(item => {
-					const listItem = document.createElement("li");
-					const link = document.createElement("a");
-					link.href = `item${item.id}.html`;
-					link.target = "_blank";
-					link.textContent = item.name;
-					listItem.appendChild(link);
-
-					// Add tags to the search result
-					const tagList = document.createElement("ul");
-					tagList.classList.add("tags");
-					item.tags.forEach(tag => {
-						const tagItem = document.createElement("li");
-						tagItem.textContent = tag;
-						tagList.appendChild(tagItem);
-					});
-					listItem.appendChild(tagList);
-
-					document.getElementById("results").appendChild(listItem);
+				function searchItems(query) {
+					document.getElementById("results").innerHTML = "";
+					const filteredItems = items.filter(item => item.id.toString().toLowerCase().includes(query.toLowerCase()) || item.name.toLowerCase().includes(query.toLowerCase()) || item.description.toLowerCase().includes(query.toLowerCase()) || item.tags.some(tag => tag.toLowerCase().includes(query.toLowerCase())));
+					if (filteredItems.length === 0) {
+						const listItem = document.createElement("li");
+						listItem.textContent = "No results found.";
+						document.getElementById("results").appendChild(listItem);
+					} else {
+						filteredItems.forEach(item => {
+							const listItem = document.createElement("li");
+							const link = document.createElement("a");
+							link.href = `item${item.id}.html`;
+							link.target = "_blank";
+							link.textContent = item.name;
+							listItem.appendChild(link);
+							const tagList = document.createElement("ul");
+							tagList.classList.add("tags");
+							item.tags.forEach(tag => {
+								const tagItem = document.createElement("li");
+								tagItem.textContent = tag;
+								tagList.appendChild(tagItem);
+							});
+							listItem.appendChild(tagList);
+							document.getElementById("results").appendChild(listItem);
+						});
+					}
+				}
+				document.getElementById("searchForm").addEventListener("submit", event => {
+					event.preventDefault();
+					searchItems(document.getElementById("search").value);
 				});
-			}
-		}
-
-		// Add event listener to search form
-		document.getElementById("searchForm").addEventListener("submit", event => {
-			event.preventDefault();
-			searchItems(document.getElementById("search").value);
-		});
-    })
+			})
+			.catch(error => {
+				console.error(error);
+				const listItem = document.createElement("li");
+				listItem.textContent = "Failed to load item data.";
+				document.getElementById("results").appendChild(listItem);
+			});
